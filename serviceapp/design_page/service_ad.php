be9c0eeeeabcaefe9b788dd737e80bdfdf4b4819
Swati-2408 <107450889+Swati-2408@users.noreply.github.com>
2022-07-01 14:22:55 +0530

    Add files via upload
    SERVICE_AD

---
+++ serviceapp/design_page/service_ad.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>service ad</title>
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+        *{
+    margin: 0;
+    padding: 0;
+    width:auto;
+    background-color: rgb(122, 191, 228)
+  }
+  #h-primary{
+    background-color: rgb(122, 191, 228)
+  }
+#heading{
+    font-size: 2.9rem;
+}   
+  #secheading{
+    font-size:1.9rem;
+  }
+  #h-secondary{
+          margin:10% 0;
+          background-color: rgb(122, 191, 228)
+  }
+  
+    </style>
+</head>
+<body>
+    <section id="h-primary">
+       <center id="heading"> HOW THE SERVICE WORKS </center>
+       <center id="secheading"> BEST SERVICE DEAL</center>
+       <div >
+       <section id="h-secondary"><section id="h-secondary"> 
+       <div class="card-deck">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">DESCRIBE YOUR TASK</h5>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="fa-solid fa-clipboard-list-check" alt="..."> 
+    <!-- <span><i class="fa-solid fa-clipboard-list-check"></i></span> -->
+    <div class="card-body">
+    <h5 class="card-title"> CHOOSE A TASKER </h5>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">LIVE SMARTER</h5>
+    </div>
+  </div>
+</div>
+       </div>
+</section>
+    </section>
+</body>
+</html>
