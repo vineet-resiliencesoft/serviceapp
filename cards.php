d605d9b7d2dceb0230d77e0fc48a8ef7a7d6ad3a
iiLuckyii <108458573+iiLuckyii@users.noreply.github.com>
2022-07-01 14:31:53 +0530

    Add files via upload

---
+++ cards.php
@@ -0,0 +1,82 @@
+><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cards</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+<style !important>
+ .cards{ margin: 5%;
+    display: flex;
+    justify-content: space-between;
+  }
+  </style>
+</head>
+<body>
+    <section style="display: flex"; justify-content:space-between; >
+  <!--  <div class="card" style="width: 18rem;">
+  <img src="https://www.cleansweepofamerica.com/wp-content/uploads/2020/10/office-cleaning-service.jpeg" alt="cleaning" class="card-img-top" alt="cleaning">
+  <div class="card-body">
+        <h5 class="card-title">Cleaning</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+  </div>
+</div>
+ <div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="https://www.cleansweepofamerica.com/wp-content/uploads/2020/10/office-cleaning-service.jpeg" alt="cleaning">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Cleaning</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div> -->
+<div class="row row-cols-1 row-cols-md-2">
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://media.istockphoto.com/photos/auto-mechanic-working-on-car-engine-in-mechanics-garage-repair-picture-id1284285171?k=20&m=1284285171&s=612x612&w=0&h=ND-oIfzIIVJuHI-Z_YGULQo7ApGXHvEDUsmmxmTnYNw=" class="card-img-top" alt="Automotive">
+      <div class="card-body">
+        <h5 class="card-title">Automotive</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://www.palmerstownhouse.com/wp-content/uploads/2021/07/Home-Plumbing-Services.jpg" class="card-img-top" alt="plumbing">
+      <div class="card-body">
+        <h5 class="card-title">Plumbing</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://www.patnarepair.com/x_images/medias/1619429985carpanters_services...2.jpg" class="card-img-top" alt="Carpenter ">
+      <div class="card-body">
+        <h5 class="card-title">Carpenter</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer</p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://dmmfacilities.com/wp-content/uploads/2022/04/electrical_service_1591799274_29c7fed8.jpg" alt="electrical">
+      <div class="card-body">
+        <h5 class="card-title">Electrical</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+</body>
+</html>
